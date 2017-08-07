e2c5481ae516be7cdf4c64ead43cea9e0b91f1d6
xqYi <yixiaoqiang1120@163.com>
2017-08-08 00:03:54 +0800

    新增类

---
+++ app/src/main/java/com/example/administrator/mvpframetestdemo/service/mvpframedemo/FrameModel.java
@@ -0,0 +1,9 @@
+package com.example.administrator.mvpframetestdemo.service.mvpframedemo;
+
+/**
+ * Created by Poison_Y on 2017/8/8.
+ * mail : yixiaoqiang1120@163.com
+ */
+
+public class FrameModel {
+}
---
+++ app/src/main/java/com/example/administrator/mvpframetestdemo/service/mvpframedemo/FramePresenter.java
@@ -0,0 +1,10 @@
+package com.example.administrator.mvpframetestdemo.service.mvpframedemo;
+
+/**
+ * Created by Poison_Y on 2017/8/8.
+ * mail : yixiaoqiang1120@163.com
+ */
+
+public class FramePresenter {
+    //fhakjshdjkfh
+}
